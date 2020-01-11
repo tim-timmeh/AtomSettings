5d8eb6e3db5ae5966c1576dd09a7cbdfd4129d29
Tim <tim_timmeh@hotmail.com>
2020-01-11 13:40:18 +0800

    screeps-ide error, reinstalled

---
+++ packages/screeps-ide/.branches/World/main.js
@@ -0,0 +1,25 @@
+'use strict';
+require('globals'); //Store global.xxx variables
+require('prototypes'); //Modify prototypes here
+//require('memory'); //build memory?
+const myFunc = require('myfunctions');
+//const _ = require('lodash'); // loads lodash? or already loaded.
+
+//--Game.spawns.Spawn1.spawnTest;  --example from require protos
+//--var roleHarvester = require("role.harvester"); -- require to variable? (Can only modify objects/prototypes not set variables from require only.)
+
+if (global.hasRespawned()) global.respawn();
+
+if(global.debug)console.log('#Global has been reset.');
+global.initRoomsMem(); // Ensure constant room features of visable rooms are in memory and structured eg. Sources
+
+module.exports.loop = function () { // Main Loop
+
+  for (let name in Memory.creeps) { // Clear memory of old creeps.
+    if (!Game.creeps[name]) {
+      delete Memory.creeps[name];
+      console.log("Clearing non-existing creep memory: ", name);
+    }
+  }
+
+}
